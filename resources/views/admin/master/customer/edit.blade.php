d947f8c3a1e0d295cafa25dba374078f13830419
Winar Joko Alexander <winaralex417@gmail.com>
2024-02-15 13:00:16 +0700

    customer

---
+++ resources/views/admin/master/customer/edit.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app2')
+@section('title', 'Customer')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Ubah Customer</h3>
+                    </div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{route('admin.master.customer.update',$customer->id)}}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="nama">Nama:</label>
+                                        <input type="text" name="nama" id="nama" class="form-control" value="{{$customer->nama}}" placeholder="Masukkan Nama">
+                                        @error('nama')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="alias">Alias:</label>
+                                        <input type="text" name="alias" id="alias" class="form-control" value="{{$customer->alias}}" placeholder="Masukkan Alias">
+                                        @error('alias')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="alamat">Alamat:</label>
+                                        <input type="text" name="alamat" id="alamat" class="form-control" value="{{$customer->alamat}}" placeholder="Masukkan Alamat">
+                                        @error('alamat')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="email">Email:</label>
+                                        <input type="text" name="email" id="email" class="form-control" value="{{$customer->email}}" placeholder="Masukkan Email">
+                                        @error('email')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="notelp">Nomor Telepon:</label>
+                                        <input type="text" name="notelp" id="notelp" class="form-control" value="{{$customer->notelp}}" placeholder="Masukkan Nomor Telepon">
+                                        @error('notelp')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group float-right">
+                                        <input class="btn btn-primary" type="submit" value="Ubah">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
