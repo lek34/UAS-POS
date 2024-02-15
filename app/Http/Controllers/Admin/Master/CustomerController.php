f9676d08cddf7cd3026c5bad521adc1e0f26a87c
Winar Joko Alexander <winaralex417@gmail.com>
2024-02-15 13:23:26 +0700

    gas

--- app/Http/Controllers/Admin/Master/CustomerController.php
+++ app/Http/Controllers/Admin/Master/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Master;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Master\Customer\CreateCustomerRequest;
 use App\Models\Customers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -29,7 +30,7 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateCustomerRequest $request)
     {
         $customers = $request->validated();
         Customers::insert($customers);
