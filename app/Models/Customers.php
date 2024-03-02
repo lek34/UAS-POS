a66c6b0eac6a7556fb43d641875d8e443c1fc95c
Winar Joko Alexander <winaralex417@gmail.com>
2024-03-02 21:00:47 +0700

    model customer dan kontrak beli

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Customers extends Model
 {
@@ -12,4 +13,9 @@ class Customers extends Model
     protected $table = 'customers';
 
     protected $fillable = ['nama','alias','alamat','email','notelp'];
+
+    public function kontrakjual(): HasMany
+    {
+        return $this->hasMany(kontrakjual::class);
+    }
 }
