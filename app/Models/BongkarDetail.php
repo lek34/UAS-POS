f5a9a1bbd5ae2e033c8816c127321a95c41746c3
Winar Joko Alexander <winaralex417@gmail.com>
2024-03-10 23:55:00 +0700

    bongkar_detail

---
+++ app/Models/BongkarDetail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class BongkarDetail extends Model
+{
+    use HasFactory;
+    protected $table = 'bongkar_details';
+    protected $fillable = ['muat_bongkar_id', 'kontrak_jual_id', 'tanggal', 'bruto', 'tarra', 'netto'];
+
+    public function muatbongkar(): BelongsTo
+    {
+        return $this->belongsTo(MuatBongkar::class);
+    }
+
+    public function kontrakjual(): BelongsTo
+    {
+        return $this->belongsTo(KontrakJual::class);
+    }
+}
