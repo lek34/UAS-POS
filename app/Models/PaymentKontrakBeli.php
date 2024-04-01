8cc16c5601d6ed0ea2b33e6d8c0b6cc63013d308
jovinkendrico <jovinkendrico@gmail.com>
2024-04-01 18:04:28 +0700

    add model and migration for payment kontrak beli

---
+++ app/Models/PaymentKontrakBeli.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentKontrakBeli extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['tanggal', 'kontrak_beli_id', 'totalbayar'];
+
+    public function kontrakbeli(): BelongsTo
+    {
+        return $this->belongsTo(KontrakBeli::class);
+    }
+}
