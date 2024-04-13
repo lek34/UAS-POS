5d060674f1d93565281edf51e84052cdd06aa83f
jovinkendrico <jovinkendrico@gmail.com>
2024-04-13 18:56:07 +0700

    fix name tanggal at show kontrakbeli

--- resources/views/admin/transaksi/kontrakbeli/show.blade.php
+++ resources/views/admin/transaksi/kontrakbeli/show.blade.php
@@ -16,7 +16,7 @@
                                 <tbody>
                                     <tr>
                                         <th>Tanggal:</th>
-                                        <td>{{ $kontrakbeli->tanggal }}</td>
+                                        <td>{{ \Carbon\Carbon::parse($kontrakbeli->tanggal)->format('d F Y') }}</td>
                                     </tr>
                                     <tr>
                                         <th>No:</th>
@@ -28,7 +28,8 @@
                                     </tr>
                                     <tr>
                                         <th>Kg:</th>
-                                        <td>{{ number_format(($kontrakbeli->kg/1000), 0, ',', '.') }} MT ({{ number_format($kontrakbeli->kg, 0, ',', '.') }} Kg)</td>
+                                        <td>{{ number_format($kontrakbeli->kg / 1000, 0, ',', '.') }} MT
+                                            ({{ number_format($kontrakbeli->kg, 0, ',', '.') }} Kg)</td>
                                     </tr>
                                     <tr>
                                         <th>Harga:</th>
@@ -38,28 +39,52 @@
                                         function terbilang($number)
                                         {
                                             $words = [
-                                                '', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh',
-                                                'sebelas', 'dua belas', 'tiga belas', 'empat belas', 'lima belas', 'enam belas', 'tujuh belas', 'delapan belas', 'sembilan belas'
+                                                '',
+                                                'satu',
+                                                'dua',
+                                                'tiga',
+                                                'empat',
+                                                'lima',
+                                                'enam',
+                                                'tujuh',
+                                                'delapan',
+                                                'sembilan',
+                                                'sepuluh',
+                                                'sebelas',
+                                                'dua belas',
+                                                'tiga belas',
+                                                'empat belas',
+                                                'lima belas',
+                                                'enam belas',
+                                                'tujuh belas',
+                                                'delapan belas',
+                                                'sembilan belas',
                                             ];
 
                                             if ($number < 20) {
                                                 return $words[$number];
                                             } elseif ($number < 100) {
-                                                return $words[($number / 10)] . ' puluh ' . $words[($number % 10)];
+                                                return $words[$number / 10] . ' puluh ' . $words[$number % 10];
                                             } elseif ($number < 200) {
                                                 return 'seratus ' . terbilang($number - 100);
                                             } elseif ($number < 1000) {
-                                                return $words[($number / 100)] . ' ratus ' . terbilang($number % 100);
+                                                return $words[$number / 100] . ' ratus ' . terbilang($number % 100);
                                             } elseif ($number < 2000) {
                                                 return 'seribu ' . terbilang($number - 1000);
                                             } elseif ($number < 1000000) {
                                                 return terbilang($number / 1000) . ' ribu ' . terbilang($number % 1000);
                                             } elseif ($number < 1000000000) {
-                                                return terbilang($number / 1000000) . ' juta ' . terbilang($number % 1000000);
+                                                return terbilang($number / 1000000) .
+                                                    ' juta ' .
+                                                    terbilang($number % 1000000);
                                             } elseif ($number < 1000000000000) {
-                                                return terbilang($number / 1000000000) . ' miliar ' . terbilang($number % 1000000000);
+                                                return terbilang($number / 1000000000) .
+                                                    ' miliar ' .
+                                                    terbilang($number % 1000000000);
                                             } elseif ($number < 1000000000000000) {
-                                                return terbilang($number / 1000000000000) . ' triliun ' . terbilang($number % 1000000000000);
+                                                return terbilang($number / 1000000000000) .
+                                                    ' triliun ' .
+                                                    terbilang($number % 1000000000000);
                                             } else {
                                                 return '';
                                             }
