8b12a4234b97e08836b55f585aad3ee1f30bbdbe
Winar Joko Alexander <winaralex417@gmail.com>
2024-04-30 15:58:10 +0700

    fix width table

--- resources/views/admin/transaksi/kontrakjual/index.blade.php
+++ resources/views/admin/transaksi/kontrakjual/index.blade.php
@@ -23,7 +23,7 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
+                        <table id="example4" class="table table-bordered table-striped">
                             <thead>
                                 <tr>
                                     <th>ID</th>
@@ -36,7 +36,7 @@
                                     <th>PPN</th>
                                     <th>Total</th>
                                     <th>Sisa Stok</th>
-                                    <th style="width: 20%">Action</th>
+                                    <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
